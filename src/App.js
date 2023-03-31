9e19f6fae7b91dcffba2f6aacc8cde56755ae842
tejashree <patwardhanteju@gmail.com>
2023-03-31 22:41:33 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Login from "./components/Login";
+import Header from './components/Header';
+import Home from './components/Home';
+import Detail from './components/Detail';
+
+import Playvid from './components/Playvid';
+import Register from './components/Register'
+import Search from './components/Search';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header/>
+      <Routes>
+          <Route exact path='/' element={<Login />}></Route>
+          <Route path="/home" element={<Home />} ></Route>
+          <Route path="/detail/:id" element={<Detail />}> </Route>  
+            <Route path='/video' element={<Playvid />}></Route>
+            <Route path='/register' element={<Register />}></Route>
+            <Route path='/search' element={< Search />}></Route> 
+            
+          </Routes>
+          
+        </Router>
+        
     </div>
   );
 }
